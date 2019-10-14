71164194f75ac7c8290aec3a7f8b7f42fcbadf4e
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2019-10-14 14:51:40 +0300

    Style changes and adds formulas

--- src/exampleFunctions.js
+++ src/exampleFunctions.js
@@ -2,56 +2,68 @@
  * f(x, y) = (x ^ 2 + y - 11) ^ 2 + (x + y ^ 2 - 7) ^ 2
  */
 const himmelblau = generate2DFunction(
-  'himmelblau',
+  "himmelblau",
   ([x, y]) => (x ** 2 + y - 11) ** 2 + (x + y ** 2 - 7) ** 2,
-  '(x ^ 2 + y - 11) ^ 2 + (x + y ^ 2 - 7) ^ 2',
-  '((x1 + l * d1) ^ 2 + (x2 + l * d2) - 11) ^ 2 + ((x1 + l * d1) + (x2 + l * d2) ^ 2 - 7) ^ 2',
-  [{ x: 3.584428, y: -1.848126 }, { x: -2.805118, y: 3.131312 }, { x: -3.779310, y: -3.283186 }, { x: 3, y: 2 }],
+  "(x ^ 2 + y - 11) ^ 2 + (x + y ^ 2 - 7) ^ 2",
+  "((x1 + l * d1) ^ 2 + (x2 + l * d2) - 11) ^ 2 + ((x1 + l * d1) + (x2 + l * d2) ^ 2 - 7) ^ 2",
+  [
+    { x: 3.584428, y: -1.848126 },
+    { x: -2.805118, y: 3.131312 },
+    { x: -3.77931, y: -3.283186 },
+    { x: 3, y: 2 }
+  ],
   [-6, 6],
-  [6, -6],
+  [6, -6]
 );
 
 /* Matyas function
  * f(x, y) = 0.26 * (x ^ 2 + y ^ 2) - 0.48 * x * y
  */
 const matyasFunction = generate2DFunction(
-  'matayasFunction',
+  "matayasFunction",
   ([x, y]) => 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y,
-  '0.26 * (x ^ 2 + y ^ 2) - 0.48 * x * y',
-  '0.26 * ((x1 + l * d1) ^ 2 + (x2 + l * d2) ^ 2) - 0.48 * (x1 + l * d1) * (x2 + l * d2)',
+  "0.26 * (x ^ 2 + y ^ 2) - 0.48 * x * y",
+  "0.26 * ((x1 + l * d1) ^ 2 + (x2 + l * d2) ^ 2) - 0.48 * (x1 + l * d1) * (x2 + l * d2)",
   [{ x: 0, y: 0 }],
   [-10, 10],
-  [10, -10],
+  [10, -10]
 );
 
 /* Exponent function
  * f(x, y) = 0.26 * (x ^ 2 + y ^ 2) - 0.48 * x * y
  */
 const exponentFunction = generate2DFunction(
-  'exponentFunction',
+  "exponentFunction",
   ([x, y]) => x * Math.exp(-(x ** 2 + y ** 2)) + (x ** 2 + y ** 2) / 20,
-  'x * exp(-(x ^ 2 + y ^ 2)) + (x ^ 2 + y ^ 2) / 20',
-  '(x1 + l * d1) * exp(-((x1 + l * d1) ^ 2 + (x2 + l * d2) ^ 2)) + ((x1 + l * d1) ^ 2 + (x2 + l * d2) ^ 2) / 20',
+  "x * exp(-(x ^ 2 + y ^ 2)) + (x ^ 2 + y ^ 2) / 20",
+  "(x1 + l * d1) * exp(-((x1 + l * d1) ^ 2 + (x2 + l * d2) ^ 2)) + ((x1 + l * d1) ^ 2 + (x2 + l * d2) ^ 2) / 20",
   [{ x: -0.669072, y: 0 }],
   [-20, 20],
-  [20, -20],
+  [20, -20]
 );
 
 /* Exponent function
  * f(x, y) = 0.26 * (x ^ 2 + y ^ 2) - 0.48 * x * y
  */
-const rosenbrockFunction  = generate2DFunction(
-  'rosenbrockFunction',
+const rosenbrock = generate2DFunction(
+  "rosenbrock",
   ([x, y]) => (1 - x) ** 2 + 100 * (y - x ** 2) ** 2,
-  '(1 - x) ^ 2 + 100 * (y - x ^ 2) ^ 2',
-  '(1 - (x1 + l * d1)) ^ 2 + 100 * ((x2 + l * d2) - (x1 + l * d1) ^ 2) ^ 2',
+  "(1 - x) ^ 2 + 100 * (y - x ^ 2) ^ 2",
+  "(1 - (x1 + l * d1)) ^ 2 + 100 * ((x2 + l * d2) - (x1 + l * d1) ^ 2) ^ 2",
   [{ x: 1, y: 1 }],
   [-2, 2],
-  [2, -2],
+  [2, -2]
 );
 
-
-function generate2DFunction(name, fastEval, equation, stepSizeEquation, minima, xDomain, yDomain) {
+function generate2DFunction(
+  name,
+  fastEval,
+  equation,
+  stepSizeEquation,
+  minima,
+  xDomain,
+  yDomain
+) {
   /* Helper to generate 2D functions in form f(x, y)
    * Inserts all required subfunctions as properties for the main function
    * - fastEval: function in plain js to increase performance (mathjs eval is slow)
@@ -64,43 +76,43 @@ function generate2DFunction(name, fastEval, equation, stepSizeEquation, minima, 
   const e = math.parse(equation);
   const f = (x, y) => {
     return fastEval([x, y]);
-  }
+  };
   f.nameStr = name;
   // f.eval = (x, y) => { return fastEval([x, y]); }
   // f.eval = (x, y) => { return f(math.matrix([x, y])); }
-  f._dx = math.derivative(e, 'x');
-  f._dy = math.derivative(e, 'y');
-  f._ddx = math.derivative(f._dx, 'x');
-  f._ddy = math.derivative(f._dy, 'y');
-  f._dxdy = math.derivative(f._dx, 'y');
-  f.gradient = (v) => {
+  f._dx = math.derivative(e, "x");
+  f._dy = math.derivative(e, "y");
+  f._ddx = math.derivative(f._dx, "x");
+  f._ddy = math.derivative(f._dy, "y");
+  f._dxdy = math.derivative(f._dx, "y");
+  f.gradient = v => {
     const [x, y] = v.toArray();
     return math.matrix([f._dx.eval({ x, y }), f._dy.eval({ x, y })]);
-  }
-  f.hessian = (v) => {
+  };
+  f.hessian = v => {
     const [x, y] = v.toArray();
-    return math.matrix(
-      [[f._ddx.eval( {x, y} ), f._dxdy.eval( {x, y} )],
-      [f._dxdy.eval( {x, y} ), f._ddy.eval( {x, y} )]]
-    )
-  }
+    return math.matrix([
+      [f._ddx.eval({ x, y }), f._dxdy.eval({ x, y })],
+      [f._dxdy.eval({ x, y }), f._ddy.eval({ x, y })]
+    ]);
+  };
 
   // Generate step size equation
   const sse = math.parse(stepSizeEquation);
-  sse.dl = math.derivative(sse, 'l');
-  sse.dl2 = math.derivative(sse.dl, 'l');
+  sse.dl = math.derivative(sse, "l");
+  sse.dl2 = math.derivative(sse.dl, "l");
 
   f.stepSizeFunction = (x, d) => {
     const [x1, x2] = x.toArray();
     const [d1, d2] = d.toArray();
 
     // Generates a step size function g(l) => f(x + l * d)
-    const g = l => sse.eval({x1, x2, d1, d2, l});
-    g.derivative = l => sse.dl.eval({x1, x2, d1, d2, l});
-    g.derivative2 = l => sse.dl2.eval({x1, x2, d1, d2, l});
+    const g = l => sse.eval({ x1, x2, d1, d2, l });
+    g.derivative = l => sse.dl.eval({ x1, x2, d1, d2, l });
+    g.derivative2 = l => sse.dl2.eval({ x1, x2, d1, d2, l });
     g.secureLimits = lambda => lambda;
     return g;
-  }
+  };
   f.xDomain = xDomain;
   f.yDomain = yDomain;
   f.minima = minima;
@@ -112,19 +124,19 @@ const visualizableFunctions = {
   himmelblau,
   matyasFunction,
   exponentFunction,
-  rosenbrockFunction,
+  rosenbrock
 };
 
 try {
   module.exports = {
     visualizableFunctions
   };
-} catch (error) { }
+} catch (error) {}
 
 /* Himmelblau
  * f(x, y) = (x ^ 2 + y - 11) ^ 2 + (x + y ^ 2 - 7) ^ 2
- * 
- * Using only javascript is approximately 100 faster than using 
+ *
+ * Using only javascript is approximately 100 faster than using
  * the version generated using math js. However, the code
  * isn't as clear and easy to edit this way.
  */
